a96df0f94a496dab4bef1c20f858a0a72fc0f112
Tom Mowad <tmowad@gmail.com>
2014-06-09 03:34:31 -0700

    Renames signup to new_subscriber.php and adds the create_subscriber.php file to handle creating the subscriber from the passed in data.

--- new_subscriber.php
+++ new_subscriber.php
@@ -6,7 +6,7 @@
     <p>You're obviously here since you've heard that Tom Mowad is all the rage these
     days, and you want to stay up to date with his latest thoughts and appearances
     around the world.</p>
-    <form action="confirm.php" method="POST">
+    <form action="create_subscriber.php" method="POST">
       <table>
         <tr>
           <td><label>Name</label></td>
